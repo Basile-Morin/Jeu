6c4f1fcc8e0913494261b524fe261ce1a22be6d2
Basile Morin <basilemorin18@gmail.com>
2025-09-17 11:36:32 +0200

    premier push 17/09/25

--- src/Displayable/Entities/Entity.java
+++ src/Displayable/Entities/Entity.java
@@ -14,7 +14,6 @@ import static java.lang.Math.signum;
 
 abstract public class Entity implements Displayable, HasHealth, HasCollision {
 
-//aaaaaaa
     protected final GamePanel gp;
     protected final int width;
     protected final int height;
